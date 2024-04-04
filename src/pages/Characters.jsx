0cbbc0e2a64e57095da6236cde94f126590d8118
Geo <geoffroymagalhaes23@gmail.com>
2024-04-04 17:29:10 +0200

    adding pagination and search bar

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -5,10 +5,15 @@ import axios from "axios";
 const Characters = () => {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(` http://localhost:3000/`);
+        const response = await axios.get(
+          ` http://localhost:3000/?name=${search}&page=${page}`
+        );
 
         // console.log(response.data.results);
         setData(response.data);
@@ -18,13 +23,30 @@ const Characters = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [search, page]);
 
   return isLoading ? (
     <p>Loading...</p>
   ) : (
     <section>
-      <div></div>
+      <div className="heroCharacters">
+        <div className="heroBackground">
+          <h1>MARVEL CHARACTERS</h1>
+          <h2>
+            Get hooked on a hearty helping of heroes and villains from the
+            humble House of Ideas!
+          </h2>
+          <input
+            placeholder="Looking for a character ?"
+            type="text"
+            name="search"
+            value={search}
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+        </div>
+      </div>
       <div className="charactersContainer">
         {data.results.map((character) => {
           return character.thumbnail.extension === "gif" ||
@@ -32,7 +54,8 @@ const Characters = () => {
               "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" ? (
             ""
           ) : (
-            <article
+            <Link
+              to={`/comics/character/${character._id}`}
               className="characterCard"
               style={{
                 backgroundImage: `url(${
@@ -40,13 +63,92 @@ const Characters = () => {
                 })`,
               }}
             >
-              <Link to={`/comics/character/${character._id}`}>
+              <div className="bottomCard">
+                {/* <h2>{character.description}</h2> */}
                 <h1>{character.name}</h1>
-                <h2>{character.description}</h2>
-              </Link>
-            </article>
+              </div>
+            </Link>
           );
         })}
+      </div>
+
+      <div className="pageButton">
+        {page !== 1 ? (
+          <button
+            onClick={() => {
+              setPage(1);
+            }}
+          >
+            &lt;&lt;
+          </button>
+        ) : (
+          <button
+            disabled
+            onClick={() => {
+              setPage(1);
+            }}
+          >
+            &lt;&lt;
+          </button>
+        )}
+
+        {page !== 1 ? (
+          <button
+            onClick={() => {
+              setPage(page - 1);
+            }}
+          >
+            &lt;
+          </button>
+        ) : (
+          <button
+            disabled
+            onClick={() => {
+              setPage(page - 1);
+            }}
+          >
+            &lt;
+          </button>
+        )}
+
+        <button>{page}</button>
+
+        {page !== (data.count / 100).toFixed() ? (
+          <button
+            onClick={() => {
+              setPage(page + 1);
+            }}
+          >
+            &gt;
+          </button>
+        ) : (
+          <button
+            disabled
+            onClick={() => {
+              setPage(page + 1);
+            }}
+          >
+            &gt;
+          </button>
+        )}
+        {page !== (data.count / 100).toFixed() ? (
+          <button
+            onClick={() => {
+              setPage((data.count / 100).toFixed());
+            }}
+          >
+            &gt;&gt;
+          </button>
+        ) : (
+          <button
+            disabled
+            onClick={() => {
+              setPage((data.count / 100).toFixed());
+            }}
+          >
+            &gt;&gt;
+          </button>
+        )}
       </div>
     </section>
   );
--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Comics = () => {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(` http://localhost:3000/comics`);
-
-        console.log(response.data);
+        const response = await axios.get(
+          ` http://localhost:3000/comics/?title=${search}&page=${page}`
+        );
+        console.log(search);
+        // console.log(response.data);
         setData(response.data);
         setIsLoading(false);
       } catch (error) {
@@ -18,13 +22,30 @@ const Comics = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [search, page]);
 
   return isLoading ? (
     <p>Loading...</p>
   ) : (
     <section>
-      <div></div>
+      <div className="heroCharacters">
+        <div className="heroBackground">
+          <h1>MARVEL COMICS</h1>
+          <h2>
+            Dive into the vibrant universe of comics, where every panel tells a
+            story and sparks your imagination to life!
+          </h2>
+          <input
+            placeholder="Looking for a comic ?"
+            type="text"
+            name="search"
+            value={search}
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+        </div>
+      </div>
       <div className="comicsContainer">
         {data.results.map((comic) => {
           return comic.thumbnail.extension === "gif" ||
@@ -32,16 +53,101 @@ const Comics = () => {
               "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" ? (
             ""
           ) : (
-            <article className="comicContainer">
-              <img
+            <article
+              className="comicCard"
+              style={{
+                backgroundImage: `url(${
+                  comic.thumbnail.path + "/portrait_uncanny.jpg"
+                })`,
+              }}
+            >
+              {/* <img
                 src={comic.thumbnail.path + "/portrait_fantastic.jpg"}
                 alt=""
-              />
+              /> */}
               <h1>{comic.title}</h1>
               <h2>{comic.description}</h2>
             </article>
           );
         })}
+      </div>
+      <div className="pageButton">
+        {page !== 1 ? (
+          <button
+            onClick={() => {
+              setPage(1);
+            }}
+          >
+            &lt;&lt;
+          </button>
+        ) : (
+          <button
+            disabled
+            onClick={() => {
+              setPage(1);
+            }}
+          >
+            &lt;&lt;
+          </button>
+        )}
+
+        {page !== 1 ? (
+          <button
+            onClick={() => {
+              setPage(page - 1);
+            }}
+          >
+            &lt;
+          </button>
+        ) : (
+          <button
+            disabled
+            onClick={() => {
+              setPage(page - 1);
+            }}
+          >
+            &lt;
+          </button>
+        )}
+
+        <button>{page}</button>
+
+        {page !== (data.count / 100).toFixed() ? (
+          <button
+            onClick={() => {
+              setPage(page + 1);
+            }}
+          >
+            &gt;
+          </button>
+        ) : (
+          <button
+            disabled
+            onClick={() => {
+              setPage(page + 1);
+            }}
+          >
+            &gt;
+          </button>
+        )}
+        {page !== (data.count / 100).toFixed() ? (
+          <button
+            onClick={() => {
+              setPage((data.count / 100).toFixed());
+            }}
+          >
+            &gt;&gt;
+          </button>
+        ) : (
+          <button
+            disabled
+            onClick={() => {
+              setPage((data.count / 100).toFixed());
+            }}
+          >
+            &gt;&gt;
+          </button>
+        )}
       </div>
     </section>
   );
